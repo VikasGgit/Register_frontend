1bf065d47f8f21df55378faa8985135892e468c8
Vikas Gupta <150171585+VikasGgit@users.noreply.github.com>
2024-07-22 06:59:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v2/user/me",
+          "https://register-backend-r8qp.onrender.com/api/v2/user/me",
           {
             withCredentials: true,
           }
